217aec9cf0c4b0235fd9590c1a53b5cb4e59fc58
azamzamy <a.zamzamy@hotmail.com>
2015-11-17 17:23:59 +0200

    validations and modifications

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
+  
+after_initialize do |comment|
+    	puts "You have created an object!"
+  	end
     belongs_to :user, dependant: :destroy
   belongs_to :photo, dependant: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ActiveRecord::Base
 has_many :friends, :through => :friendships
 has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
 has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
+
+
+	validates :email, :fname,  presence: true
+	validates :name, length: { minimum: 2 }
+	validate :that_born_on_is_not_in_the_future
+
+	 def that_born_on_is_not_in_the_future
+	 	self.errors.add :yob, 'is in the future' 
+	 unless self.yob <= Date.today
+
+
 end
