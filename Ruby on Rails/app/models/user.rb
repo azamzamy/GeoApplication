b03d86b0555bac351751dbec30bb4cee0390cffc
ahmedtarek- <ahmed.t.abdelfattah@gmail.com>
2015-12-14 20:26:00 +0200

    backend is now without major errors

--- Ruby on Rails/app/models/user.rb
+++ Ruby on Rails/app/models/user.rb
@@ -5,16 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
          has_many :friendships
-has_many :friends, :through => :friendships
-has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+		has_many :friends, :through => :friendships
+		has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+		has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
 
 
-	validates :email, :fname,  presence: true
-	validates :name, length: { minimum: 2 }
-	validate :that_born_on_is_not_in_the_future
-
+	
 	 # def that_born_on_is_not_in_the_future
 	 # 	self.errors.add :yob, 'is in the future' 
 	 # unless self.yob <= Date.today
