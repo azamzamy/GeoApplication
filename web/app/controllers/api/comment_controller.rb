b5242c56bceab2e47d2cc1fae4b7850aa40402ee
Hend Hesham Seif <hend.heshamseif@hotmail.com>
2015-12-15 03:54:54 +0200

    comment  controller

--- web/app/controllers/api/comment_controller.rb
+++ web/app/controllers/api/comment_controller.rb
@@ -2,7 +2,34 @@ class Api::CommentController < Api::BaseController
 	before_action :authenticate_user!, only: :buy
 
 	def index
-	    respond_with @comments = Comment.all
+		@photo = Photo.find(params[:photo_id])
+	    respond_with @comments = @photo.comments.all
 	end
+#create
+def create
+  @user = User.find(params[:user_id])
+  @photo = @user.photos.find(params[:photo_id])
+  @comment = @photo.comments.create(comment_params)
+@comment.user_id= @photo.user_id
+  if @comment.save
+format.json{render json: @comment , status: :created , location: @comment}
+else
+	format.json{render json: @comment.errors , status: :unprocessable_entity}
+  end
+end
+
+
+def destroy
+  @photo = Photo.find(params[:photo_id])
+   @comment = @photo.comments.find(params[:id])
+   @comment.destroy
+ render json: {}, status: :no_content
+ 
+end
+
+protected
+  def comment_params
+    params.require(:comment).permit(:content, :user_id , :photo_id)
+end
 	
 end
